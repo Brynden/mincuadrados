0b9c4e88670d6d5686ac017cdb020ac7ff88e612
Wolf1509 <wolf1509@gmail.com>
2014-06-13 20:41:41 -0400

    llenar matriz3
    validacion de errores

--- llenar matriz2.py
+++ llenar matriz2.py
@@ -1,11 +1,29 @@
 from numpy import*
 
-filas = int(input("Cantidad de Filas: "))
-columnas = int(input("Cantidad de Columnas: "))
+print("Ingrese dimensiones de la matriz(Filas x Columnas) entre 1 y 5")
+while True:
+        try:
+            filas = int(input('Filas:'))
+            columnas = int(input('Columnas:'))
+        except ValueError:
+            print("Intentelo nuevamente")
+        else:
+            if filas < 1 or filas > 5 or columnas < 1 or columnas > 5:
+                print("Intentelo nuevamente")
+            else:
+                break
+        
 matriz = empty((filas,columnas))
+
 for i in range(filas):
         for j in range(columnas):
-                matriz[i][j]=int(input("Elemento %d,%d : " % (i,j) ))
-                  
+                while True:
+                        try:
+                                valor=float(input("Elemento %d,%d : " % (i+1,j+1) ))
+                        except ValueError:
+                                print("Intentelo nuevamente")
+                        else:
+                                matriz[i][j]=valor
+                                break
 print (matriz)
 
