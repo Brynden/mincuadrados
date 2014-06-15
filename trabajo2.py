c84a74a27c80a5a25d711374b8880281fd992590
Wolf1509 <wolf1509@gmail.com>
2014-06-15 17:17:24 -0400

    recursividad menus
    +menus recursivos
    *ya no se que mas se pueda agregar o mejorar
    *falta comentar

--- trabajo2.py
+++ trabajo2.py
@@ -1,6 +1,6 @@
 from numpy import *
 import matplotlib.pyplot as plt
-set_printoptions(precision=4)
+set_printoptions(precision=6)
 
 def mult(matriz1,matriz2):
     if matriz1.shape[1] != matriz2.shape[0]:
@@ -77,6 +77,7 @@ def mincuad(a,b):
     ata2=inversa(ata)
     return(mult(ata2,atb))
 
+#grf=
 #1recta
 #2cuadratica
 #3recta y cuadratica
@@ -95,7 +96,6 @@ def aprox(pts,grf):
     plt.xlabel("X")
     plt.ylabel("Y")    
     plt.title("Aproximación polinomial de puntos metodo mínimos cuadrados")
-    print(grf)
     A1=array([[x1[j], 1] for j in range(n)])
     X1=mincuad(A1,y)
     xr=linspace(amin(x1)-2,amax(x1)+2,3)
@@ -114,52 +114,46 @@ def aprox(pts,grf):
         x3=x1**3
         A3=array([[x3[j], x2[j], x1[j], 1] for j in range(n)])
         X3=mincuad(A3,y)
-        xcb=linspace(amin(x1)-3,amax(x1)+3,60)
+        xcb=linspace(amin(x1)-1,amax(x1)+1,60)
         cub=X3[0]*x1**3+X3[1]*x1**2+X3[2]*x1+X3[3]
         cub2=X3[0]*xcb**3+X3[1]*xcb**2+X3[2]*xcb+X3[3]
         d3=sum((cub-y1)**2)
     if grf==6 or grf==4:
+        print("La funcion que minimiza la suma de los cuadrados de los residuos (scr) es: ")
         if grf==6 and d3<d2 and d3<d1:
             plt.plot(xcb,cub2,"g",label=r'$%.3f*x^3%+.3f*x^2%+.3f*x%+.3f" $'% (X3[0],X3[1],X3[2],X3[3]))
-            print("La curva que mejor se aproxima es la ecuacion cúbica: \n %.3f*x^3%+.3f*x^2%+.3f*x%+.3f" % (X3[0],X3[1],X3[2],X3[3]))
+            print("La ecuacion cúbica: \n %.6f*x^3%+.6f*x^2%+.6f*x%+.6f" % (X3[0],X3[1],X3[2],X3[3]))
+            print("scr para funcion cúbica= %f" % (d3))
         elif d1<=d2:
             plt.plot(xr,recta2,"b-",label=r'$y=%10.3f*x%+.3f" $'% (X1[0],X1[1]))
-            print("La mejor aproximación es la recta: \n %10.3f*x%+.3f" % (X1[0],X1[1]))
+            print("La recta: \n %10.6f*x%+.6f" % (X1[0],X1[1]))
         elif d2<d1:            
             plt.plot(xcd,cuad2,"r",label=r'$%.3f*x^2%+.3f*x%+.3f" $'% (X2[0],X2[1],X2[2]))
-            print("La curva que mejor se aproxima es la ecuacion cuadrática\n %.3f*x^2%+.3f*x%+.3f" % (X2[0],X2[1],X2[2]))
+            print("La curva que mejor se aproxima es la ecuacion cuadrática\n %.6f*x^2%+.6f*x%+.6f" % (X2[0],X2[1],X2[2]))
+        print("scr para funcion cuadrática= %f \nscr para la recta= %f" % (d2,d1))
     elif grf==7 or grf==3:
         if grf==7:
-            print(int(X3[0]))
             plt.plot(xcb,cub2,"g",label=r'$%.3f*x^3%+.3f*x^2%+.3f*x%+.3f" $'% (X3[0],X3[1],X3[2],X3[3]))
-            print("La ecuacion cúbica es: \n %.3f*x^3%+.3f*x^2%+.3f*x%+.3f" % (X3[0],X3[1],X3[2],X3[3]))
+            print("La ecuacion cúbica es: \n %.6f*x^3%+.6f*x^2%+.6f*x%+.6f" % (X3[0],X3[1],X3[2],X3[3]))
         plt.plot(xcd,cuad2,"r",label=r'$%.3f*x^2%+.3f*x%+.3f" $'% (X2[0],X2[1],X2[2]))
-        print("La ecuacion cuadrática es: \n %.3f*x^2%+.3f*x%+.3f" % (X2[0],X2[1],X2[2]))
+        print("La ecuacion cuadrática es: \n %.6f*x^2%+.6f*x%+.6f" % (X2[0],X2[1],X2[2]))
         plt.plot(xr,recta2,"b-",label=r'$%10.3f*x%+.3f" $'% (X1[0],X1[1]))
-        print("La recta es: \n %.3f*x%+.3f" % (X1[0],X1[1]))
+        print("La recta es: \n %.6f*x%+.6f" % (X1[0],X1[1]))
     elif grf==1:
         plt.plot(xr,recta2,"b-",label=r'$%10.3f*x%+.3f" $'% (X1[0],X1[1]))
-        print("La recta es: \n %.3f*x%+.3f" % (X1[0],X1[1]))
+        print("La recta es: \n %.6f*x%+.6f" % (X1[0],X1[1]))
     elif grf==2:
         plt.plot(xcd,cuad2,"r",label=r'$%.3f*x^2%+.3f*x%+.3f" $'% (X2[0],X2[1],X2[2]))
-        print("La ecuacion cuadrática es: \n %.3f*x^2%+.3f*x%+.3f" % (X2[0],X2[1],X2[2]))
+        print("La ecuacion cuadrática es: \n %.6f*x^2%+.6f*x%+.6f" % (X2[0],X2[1],X2[2]))
     else:
         plt.plot(xcb,cub2,"g",label=r'$%.3f*x^3%+.3f*x^2%+.3f*x%+.3f" $'% (X3[0],X3[1],X3[2],X3[3]))
-        print("La ecuacion cúbica es: \n %.3f*x^3%+.3f*x^2%+.3f*x%+.3f" % (X3[0],X3[1],X3[2],X3[3]))
-    
-          
+        print("La ecuacion cúbica es: \n %.6f*x^3%+.6f*x^2%+.6f*x%+.6f" % (X3[0],X3[1],X3[2],X3[3]))
+              
     plt.legend(loc = 4)            
     plt.show()
-##    if grf==4:
-##        if d1<=d2 and d1<=d3:
-##            return(plt.show(pl1))
-##        elif d2<d1 and d2<=d3:
-##            return(plt.show(pl2))
-##        else:
-##            return(plt.show(pl3))
     
 def ingreso (matrizA,matrizB):
-    print("Ingrese dimensiones de la matriz(Filas x Columnas) entre 1 y 5")
+    print("Ingrese dimensiones de la matriz(max 5x5)")
     while True:
         try:
             filas = int(input('Filas:'))
@@ -167,7 +161,7 @@ def ingreso (matrizA,matrizB):
         except ValueError:
             print("Intentelo nuevamente")
         else:
-            if filas < 1 or filas > 5 or columnas < 1 or columnas > 5:
+            if filas < 2 or filas > 5 or columnas < 2 or columnas > 5:
                 print("Intentelo nuevamente")
             else:
                 break
@@ -198,7 +192,10 @@ def ingreso (matrizA,matrizB):
                 matrizB[i]=valor
                 break
     matrizB=matrizB.reshape(filas,1)
-    print (matrizA,matrizB)
+    print ("A=")
+    print (matrizA)
+    print ("B=",)
+    print (matrizB)
     return matrizA,matrizB
 
 def ingpto(puntos):
@@ -236,65 +233,72 @@ def ingpto(puntos):
     return(puntos,len(unique(puntos[:,0])))
 
     
-print("Aproximacion de Minimos cuadrados \n 1)Aproximacion para sistemas de ecuaciones \n 2)Aproximacion a partir de puntos")
-elije=input()
-if elije=="1":
-    matrizA=matrizB=array
-    matrizA,matrizB=ingreso(matrizA,matrizB)
-    if matrizA.shape[1]==linalg.matrix_rank(matrizA):
-        print ("Soluciones por minimos cuadrados: ")
-        print (mincuad(matrizA,matrizB))
-    else:
-        print("No tiene solucion por minimos cuadrados")
+print("Aproximación de Minimos cuadrados \n 1)Sistemas de ecuaciones \n 2)Aproximacion a partir de puntos")
 
-#1recta
-#2cuadratica
-#3recta y cuadratica
-#4calcular recta y cuadra
-#5cubica
-#6calcular todas
-#7 graficar todas
-
-else:
-    puntos=array
-    puntos,cx=ingpto(puntos)
-    grf=0
-    if cx==2:
-        grf=1
-        aprox(puntos,grf)
-    elif cx==3:
-        print("Aproximar los puntos a: \n 1)Recta \n 2)Función Cuadrática \n 3)Ambas \n 4)Calcular la que mejor aproxime")
-        while True:
-            try:
-                grf=int(input())
-            except ValueError:
-                        print("Intentelo nuevamente")
-            else:
-                if grf==1 or grf==2 or grf==3 or grf==4:
-                    break
-                else:
-                    print("Ingrese una opción válida")
-        aprox(puntos,grf)
+while True:
+    try:
+        elije=int(input())
+    except ValueError:
+        print("Intentelo nuevamente")
     else:
-        print("Aproximar los puntos a: \n 1)Recta \n 2)Función Cuadrática \n 3)Función Cubica \n 4)Calcular la que mejor aproxime \n 5)Todas")
-        while True:
-            try:
-                grf=int(input())
-            except ValueError:
-                        print("Intentelo nuevamente")
+        if elije!=1 and elije!=2 and elije !=3:
+            print("Ingrese una opción válida")
+        elif elije==1:
+            matrizA=matrizB=array
+            matrizA,matrizB=ingreso(matrizA,matrizB)
+            if matrizA.shape[1]==linalg.matrix_rank(matrizA):
+                print ("Soluciones por minimos cuadrados: ")
+                print (mincuad(matrizA,matrizB))
             else:
-                if grf==1 or grf==2:
-                    break
-                elif grf==3:
-                    grf=5
-                    break
-                elif grf==4:
-                    grf=6
-                    break
-                elif grf==5:
-                    grf=7
-                    break
-                else:
-                    print("Ingrese una opción válida")
-        aprox(puntos,grf)
-        
+                print("El rango de la matriz A es distinto al número de columnas. \nNo tiene solución única por minimos cuadrados")
+
+
+        elif elije==2:
+            puntos=array
+            puntos,cx=ingpto(puntos)
+            grf=0
+            sw1=0
+            if cx==2:
+                grf=1
+                aprox(puntos,grf)
+            elif cx==3:       
+                while sw1==0:
+                    print("Aproximar los puntos a: \n 1)Recta \n 2)Función Cuadrática \n 3)Ambas \n 4)Calcular la que mejor aproxime \n 5)Salir")
+                    try:
+                        grf=int(input())
+                    except ValueError:
+                                print("Intentelo nuevamente")
+                    else:
+                        if grf!=1 and grf!=2 and grf!=3 and grf!=4 and grf!=5:
+                            print("Ingrese una opción válida")
+                        elif grf!=5:
+                            aprox(puntos,grf)
+                        else:
+                            sw1=1
+            else:
+                while sw1==0:
+                    print("Aproximar los puntos a: \n 1)Recta \n 2)Función Cuadrática \n 3)Función Cubica \n 4)Calcular la que mejor aproxime \n 5)Todas \n 6)Salir")
+                    try:
+                        grf=int(input())
+                    except ValueError:
+                                print("Intentelo nuevamente")
+                    else:
+                        if grf!=1 and grf!=2 and grf!=3 and grf!=4 and grf!=5 and grf!=6:
+                            print("Ingrese una opción válida")
+                        elif grf!=6:    
+                            if grf==5:
+                                grf=7
+                            if grf==4:
+                                grf=6
+                            if grf==3:
+                                grf=5
+                            aprox(puntos,grf)
+                        else:
+                            sw1=1
+        else:
+            break
+    input()
+    print("\n\n 1)Sistemas de ecuaciones \n 2)Aproximacion a partir de puntos \n 3)Salir")
+
+
+            
